9b88ae7538272073284ab5a6043c17b285dc2325
EduPatriots <66177547+EduPatriots@users.noreply.github.com>
2022-04-24 23:18:48 -0500

    Practica 4

---
+++ Tarea 4.py
@@ -0,0 +1,47 @@
+from pickle import FALSE, TRUE
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data=pd.read_csv('spreadspoke_scores.csv')
+dataTeams=pd.read_csv('nfl_teams.csv')
+# dataStadiums=pd.read_csv('nfl_stadiums.csv')
+equiTeams = {}
+
+data=data[['schedule_season','schedule_playoff','team_home','score_home','score_away','team_away','team_favorite_id','spread_favorite']]
+
+data=data.dropna()
+
+data = data.drop(2268,0)
+
+for _, row in dataTeams.iterrows():
+    equiTeams[row['team_name']] = row['team_id']
+
+data['team_home'] = data['team_home'].map(equiTeams)
+data['team_away'] = data['team_away'].map(equiTeams)
+
+data=data[(data['schedule_playoff']==False)]
+
+#Aquí empieza la tarea 3
+
+handicap_prom = data.agg({"spread_favorite":['mean']})
+# print(handicap_prom)
+
+max_handicap = data.agg({"spread_favorite":['idxmin']})
+
+max_handicap = max_handicap['spread_favorite'].astype(str).astype(int)
+
+# print(data.loc[int(max_handicap)], "\nLa apuesta con mayor handicap en la historia la ganó quien aposto por el contrario\n")
+
+min_handicap = data.agg({"spread_favorite":['idxmax']})
+
+min_handicap = min_handicap['spread_favorite'].astype(str).astype(int)
+
+# print(data.loc[int(min_handicap)], "\nSorprendentemente existió una apuesta al empate a pesar de la rareza de estos en la liga")
+
+#Aquí empieza la tarea 4
+
+disparidad_anual = data[['schedule_season','spread_favorite']].groupby(pd.Grouper(key="schedule_season")).mean()
+
+plt.plot(disparidad_anual)
+plt.show()
+
